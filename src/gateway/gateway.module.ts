3809095433f334fad7e7a706ed75983f61adf03f
h3nrzi <rezaeig22@gmail.com>
2025-09-28 19:41:35 +0330

    feat: Add GatewayController and integrate with GatewayModule for improved request handling

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,4 +1,11 @@
 import { Module } from "@nestjs/common";
 
-@Module({})
+import { RateLimiterModule } from "../rate-limiter/rate-limiter.module";
+
+import { GatewayController } from "./gateway.controller";
+
+@Module({
+  imports: [RateLimiterModule],
+  controllers: [GatewayController],
+})
 export class GatewayModule {}
--- src/rules/rules.controller.ts
+++ src/rules/rules.controller.ts
@@ -1,4 +1,22 @@
 import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+  ValidationPipe,
+  NotFoundException,
+} from "@nestjs/common";
+
+import { SkipRateLimit } from "../rate-limiter/decorators/skip-rate-limit.decorator";
+
+import { CreateRateLimitRuleDto } from "./dtos/create-rule.dto";
+import { RulesService } from "./rules.service";
 
 @Controller("rules")
 export class RulesController {}
